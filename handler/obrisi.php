0f11dc35f5572b1edddc7db26e979aaba9b5737f
Mario64NP <mj20171055@student.fon.bg.ac.rs>
2022-11-19 22:51:19 +0100

    Formatiranje

--- handler/obrisi.php
+++ handler/obrisi.php
@@ -3,7 +3,6 @@ require "../DatabaseBroker.php";
 require "../model/Proizvod.php";
 
 if(isset($_POST['proizvodID'])){
-    
     $rezultat = Proizvod::obrisiProizvod($_POST['proizvodID'], $conn);
     if($rezultat){
         echo 'Success';
